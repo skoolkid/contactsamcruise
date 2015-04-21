dbc704ce9d38301fc46948151a123b5a0f8fe63e
Richard Dymond <rjdymond@gmail.com>
2015-04-21 19:07:26 -0300

    Remove tests/csctest.py

--- utils/write-tests.py
+++ utils/write-tests.py
@@ -1,63 +1,68 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import sys
 import os
 
-def write_tests(superclass_name, class_name, options_list, games):
-    print('from csctest import {}'.format(superclass_name))
-    print('')
-    print('class {}({}):'.format(class_name, superclass_name))
-    for game in games:
-        for options in options_list:
-            method_name_suffix = options.replace('-', '_').replace(' ', '')
-            method_name = 'test_{}{}'.format(game, method_name_suffix)
-            print("    def {}(self):".format(method_name))
-            print("        self.write_{}('{}')".format(game, options))
-            print("")
-
-def get_asm_options_list():
-    options_list = []
-    for b in ('', '-D', '-H'):
-        for c in ('', '-l', '-u'):
-            for f in ('', '-f 1', '-f 2', '-f 3'):
-                for p in ('', '-s', '-r'):
-                    options_list.append('{} {} {} {}'.format(b, c, f, p).strip())
-    return options_list
-
-def get_ctl_options_list():
-    options_list = []
-    for w in ('', '-w b', '-w bt', '-w btd', '-w btdr', '-w btdrm', '-w btdrms', '-w btdrmsc'):
-        for h in ('', '-h'):
-            for a in ('', '-a'):
-                for b in ('', '-b'):
-                    options_list.append('{} {} {} {}'.format(w, h, a, b).strip())
-    return options_list
-
-def get_html_options_list():
-    options_list = []
-    for b in ('', '-D', '-H'):
-        for c in ('', '-u', '-l'):
-            options_list.append('{} {}'.format(b, c).strip())
-    return options_list
-
-TEST_TYPES = {
-    'asm': get_asm_options_list(),
-    'ctl': get_ctl_options_list(),
-    'html': get_html_options_list(),
-    'sft': ('', '-h', '-b', '-h -b')
-}
-
-###############################################################################
-# Begin
-###############################################################################
-if not (len(sys.argv) == 2 and sys.argv[1] in TEST_TYPES):
-    sys.stderr.write("Usage: {} asm|ctl|html|sft\n".format(os.path.basename(sys.argv[0])))
+SKOOLKIT_HOME = os.environ.get('SKOOLKIT_HOME')
+if not SKOOLKIT_HOME:
+    sys.stderr.write('SKOOLKIT_HOME is not set; aborting\n')
     sys.exit(1)
-test_type = sys.argv[1]
-superclass_name = '{}TestCase'.format(test_type.capitalize())
-class_name = 'ContactSamCruise{}Test'.format(test_type.capitalize())
-if test_type == 'asm':
-    games = ['csc' + suffix for suffix in ('', '_load', '_save', '_start')]
-else:
-    games = ['csc']
-write_tests(superclass_name, class_name, TEST_TYPES[test_type], games)
+if not os.path.isdir(SKOOLKIT_HOME):
+    sys.stderr.write('SKOOLKIT_HOME={}: directory not found\n'.format(SKOOLKIT_HOME))
+    sys.exit(1)
+sys.path.insert(0, '{}/tools'.format(SKOOLKIT_HOME))
+from testwriter import write_tests
+
+SKOOL = '../sources/csc.skool'
+
+SNAPSHOT = '../build/contact_sam_cruise.z80'
+
+OUTPUT = """Creating directory {odir}
+Using skool file: ../sources/csc.skool
+Using ref files: ../sources/csc.ref, ../sources/csc-bugs.ref, ../sources/csc-changelog.ref, ../sources/csc-data.ref, ../sources/csc-facts.ref, ../sources/csc-glossary.ref, ../sources/csc-graphics.ref, ../sources/csc-pages.ref, ../sources/csc-pokes.ref
+Parsing ../sources/csc.skool
+Creating directory {odir}/contact_sam_cruise
+Copying {SKOOLKIT_HOME}/skoolkit/resources/skoolkit.css to {odir}/contact_sam_cruise/skoolkit.css
+Copying ../sources/csc.css to {odir}/contact_sam_cruise/csc.css
+  Writing disassembly files in contact_sam_cruise/asm
+  Writing contact_sam_cruise/maps/all.html
+  Writing contact_sam_cruise/maps/routines.html
+  Writing contact_sam_cruise/maps/data.html
+  Writing contact_sam_cruise/maps/messages.html
+  Writing contact_sam_cruise/buffers/gbuffer.html
+  Writing contact_sam_cruise/graphics/graphics.html
+  Writing contact_sam_cruise/graphics/asstart0.html
+  Writing contact_sam_cruise/graphics/asstart1.html
+  Writing contact_sam_cruise/graphics/asstart2.html
+  Writing contact_sam_cruise/graphics/asstart3.html
+  Writing contact_sam_cruise/graphics/asstart4.html
+  Writing contact_sam_cruise/graphics/as.html
+  Copying ../sources/tiles.js to {odir}/contact_sam_cruise/tiles.js
+  Writing contact_sam_cruise/graphics/astiles/astiles.html
+  Writing contact_sam_cruise/buffers/cbuffer.html
+  Writing contact_sam_cruise/tables/keys.html
+  Writing contact_sam_cruise/maps/command_lists.html
+  Writing contact_sam_cruise/reference/walkthrough.html
+  Writing contact_sam_cruise/graphics/playarea.html
+  Writing contact_sam_cruise/graphics/playarea_objects.html
+  Writing contact_sam_cruise/graphics/glitches.html
+  Writing contact_sam_cruise/reference/changelog.html
+  Writing contact_sam_cruise/reference/bugs.html
+  Writing contact_sam_cruise/reference/facts.html
+  Writing contact_sam_cruise/reference/glossary.html
+  Writing contact_sam_cruise/reference/pokes.html
+  Parsing ../sources/csc-load.skool
+    Writing contact_sam_cruise/load/load.html
+    Writing disassembly files in contact_sam_cruise/load
+  Parsing ../sources/csc-save.skool
+    Writing contact_sam_cruise/save/save.html
+    Writing disassembly files in contact_sam_cruise/save
+  Parsing ../sources/csc-start.skool
+    Writing contact_sam_cruise/start/start.html
+    Writing disassembly files in contact_sam_cruise/start
+  Writing contact_sam_cruise/index.html"""
+
+HTML_WRITER = '../sources:samcruise.ContactSamCruiseHtmlWriter'
+
+ASM_WRITER = '../sources:samcruise.ContactSamCruiseAsmWriter'
+
+write_tests(SKOOL, SNAPSHOT, OUTPUT, HTML_WRITER, ASM_WRITER)
